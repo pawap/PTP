05c54ada414dbf9dd65e5d2cfb470e11ab3ec747
6runge <28950411+6runge@users.noreply.github.com>
2018-04-08 23:34:06 +0200

    [TASK] Bugfixing...not done yet

---
+++ src/mydraw/MyColor.java
@@ -0,0 +1,44 @@
+package mydraw;
+
+import java.awt.Color;
+
+public enum MyColor {
+	BLACK, GREEN, RED, BLUE, WHITE; 
+
+	public static boolean isAColor(String str) {
+		for (MyColor c : MyColor.values()) {
+			if (c.toString().equals(str)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static Color stringToColor(String str) throws ColorException{
+
+		String col = str.toUpperCase();
+
+
+		if (MyColor.isAColor(col)) {
+			switch (col) {
+			case "BLACK": return Color.black;
+
+			case "GREEN": return Color.green;
+
+			case "RED":   return Color.red;
+
+			case "BLUE":  return Color.blue;
+
+			case "WHITE": return Color.white;
+			
+			default:      System.out.println("The String reads " + col);
+			return Color.pink;
+			}
+		}
+		else {
+			System.out.println(col);
+			throw new ColorException();
+		}
+	}
+}
+
